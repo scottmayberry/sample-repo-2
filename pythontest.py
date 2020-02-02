2016a4b51da2bd5e394b749c469e08d5cc49d2b5
Scott Mayberry <sctmayberry@gmail.com>
2020-02-02 14:48:52 -0500

    added some code

--- pythontest.py
+++ pythontest.py
@@ -1,14 +1,7 @@
 
-
-import math
-import os
-import sys
-
 import requests
 
 
-print(sys.version)
-print(sys.executable)
-
 r = requests.get('https://sctmayberry.com')
 print(r.status_code)
+print(r.ok)
